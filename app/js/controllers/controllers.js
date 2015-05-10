7f13a33d7bc56c38e8434ed2460febc2f7da42f9
Caitlin <caitlin.colbert@gmail.com>
2015-05-10 11:26:18 -0700

    finishing countries view

--- app/js/controllers/controllers.js
+++ app/js/controllers/controllers.js
@@ -3,7 +3,7 @@ angular.module("countries")
 .controller('countriesController', ['$scope', 'countries',
   function($scope, countries) {
     countries.getCountries().then(function(result) {
-      console.log(result);
-      $scope.countries = result.geonames;
+      console.log(result.geonames.country);
+      $scope.countries = result.geonames.country;
     });
 }]);
--- app/js/services/api.js
+++ app/js/services/api.js
@@ -9,7 +9,14 @@ angular.module('ApiCalls', [])
       getCountries: function() {
         var defer = $q.defer();
         var url = api_prefix + "countryInfo?username=" + username;
-        $http.get(url)
+        $http.get(url,
+          {transformResponse: function(data) {
+              var x2js = new X2JS();
+              var json = x2js.xml_str2json(data);
+                return json;
+              }
+            }
+          )
           .success(function(data) {
             defer.resolve(data);
           });
