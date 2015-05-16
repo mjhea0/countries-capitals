41aa9dd21c501408a19f558f08a178d6063f7329
Caitlin <caitlin.colbert@gmail.com>
2015-05-16 10:41:10 -0700

    updating styles and loading animation

--- app/js/main.js
+++ app/js/main.js
@@ -17,4 +17,18 @@ angular.module('countries', ['ApiCalls', 'ngRoute', 'ngAnimate'])
       .otherwise({
          redirectTo: '/'
       });
-}]);
+}])
+
+.run(function($rootScope, $location, $timeout) {
+    $rootScope.$on('$routeChangeError', function() {
+        $location.path("/error");
+    });
+    $rootScope.$on('$routeChangeStart', function() {
+        $rootScope.isLoading = true;
+    });
+    $rootScope.$on('$routeChangeSuccess', function() {
+      $timeout(function() {
+        $rootScope.isLoading = false;
+      }, 1000);
+    });
+});
