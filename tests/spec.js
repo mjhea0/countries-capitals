b32600289db65461caaf9735feebbaaa940b861b
Michael Herman <hermanmu@gmail.com>
2015-05-17 22:05:01 -0600

    code review

--- tests/spec.js
+++ tests/spec.js
@@ -1,74 +1,78 @@
 describe("countries routes", function() {
 
-    beforeEach(function() {
-        module('countries');
+  beforeEach(function() {
+    module('countries');
+  });
+
+  it('should load home page', function() {
+  inject(function($route, $location, $rootScope, $httpBackend) {
+    var route = $route.routes['/'];
+    $httpBackend.whenGET(route.templateUrl).respond('...');
+
+    $rootScope.$apply(function() {
+      $location.path(route.originalPath);
     });
 
-    it('should load home page', function() {
-    inject(function($route, $location, $rootScope, $httpBackend) {
-      var route = $route.routes['/'];
-       $httpBackend.whenGET(route.templateUrl).respond('...');
-
-       $rootScope.$apply(function() {
-         $location.path(route.originalPath);
-      });
-
-      expect($route.current.templateUrl).toBe("./partials/home.html");
-      });
+    expect($route.current.templateUrl).toBe("./partials/home.html");
     });
+  });
 
-    it('should load countries page', function() {
+  it('should load countries page', function() {
     inject(function($route, $location, $rootScope, $httpBackend) {
       var route = $route.routes['/countries'];
-       $httpBackend.whenGET(route.templateUrl).respond('...');
+      $httpBackend.whenGET(route.templateUrl).respond('...');
 
-       $rootScope.$apply(function() {
-         $location.path(route.originalPath);
+      $rootScope.$apply(function() {
+        $location.path(route.originalPath);
       });
 
       expect($route.current.templateUrl).toBe("./partials/countries.html");
-      });
     });
+  });
+
 });
 
 
 describe('countriesData', function () {
 
-    beforeEach(function () {
-      module('ApiCalls');
-    });
+  beforeEach(function () {
+    module('ApiCalls');
+  });
 
   it('should return API data', function() {
-    inject(function(countriesData, $rootScope, $httpBackend) {
-        $httpBackend.expect('GET', 'http://api.geonames.org/countryInfo?username=ccolbert').respond(200);
-        var status = false;
-
-        $rootScope.$digest();
-        $httpBackend.flush();
-        expect(status).toBe(true);
-        $httpBackend.verifyNoOutstandingRequest();
-        });
+    inject(function(countriesData, $httpBackend, $route, $q) {
+      $httpBackend.expect('GET', 'http://api.geonames.org/countryInfo?username=ccolbert').respond(200);
+      var status = false;
+      $rootScope.$digest();
+      $httpBackend.flush();
+      expect(status).toBe(true);
+      $httpBackend.verifyNoOutstandingRequest();
     });
+  });
+
 });
 
 describe('countries controller', function () {
 
-    var controller = null;
-    $scope = null;
+  var controller = null;
+  $scope = null;
 
-    beforeEach(function() {
-        module('countries');
+  beforeEach(function() {
+    module('countries');
+  });
+
+  beforeEach(inject(function ($controller, $rootScope) {
+    $scope = $rootScope.$new();
+    controller = $controller('countriesController', {
+      $scope: $scope
     });
-
-    beforeEach(inject(function ($controller, $rootScope) {
-        $scope = $rootScope.$new();
-        controller = $controller('countriesController', {
-            $scope: $scope
-        });
-    }));
+  }));
 
   it('should return list of countries', function () {
-    // $scope.getCountries();
-    expect($scope.countries).toBeDefined();
+    inject(function(countriesData) {
+      console.log($scope.countries);
+      expect($scope.countries).toBeDefined();
+    });
   });
+
 });
